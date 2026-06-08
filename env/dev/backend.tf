c2398f7e32523eb7822827d4c9f1b885fceab221
Siddhant-Simform <siddhant.rathod@simformsolutions.com>
2026-06-08 17:02:43 +0530

    Sample Commit

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-sentinelops-iac-shared-cin"
     storage_account_name = "sentinelopssastatestorage"
     container_name       = "sentinelops-state-files"
-    key                  = "Env/Dev/terraform.tfstate"
+    key                  = "env/dev/terraform.tfstate"
   }
 }
